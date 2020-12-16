0fd66629db5a5f8f175fc297752209b0bb94c844
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-15 20:46:29 -0600

    add heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -56,5 +57,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(3001, () => {
-	console.log('API server now on port 3001!');
+	console.log('API server now on port ${PORT}!');
 });
