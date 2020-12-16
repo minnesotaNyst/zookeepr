d15f4a79ce045b5377972de21a61de1dc5440090
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-15 20:52:24 -0600

    changes to the port console log

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(PORT, () => {
-	console.log('API server now on port ${PORT}!');
-});
+	console.log(`API server now on port ${PORT}!`);
+  });
